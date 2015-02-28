92261f1cc33c868c10460dc2e04efa10ea32b6aa
Scott Thompson <easco@mac.com>
2015-02-28 17:36:26 -0600

    support multiline values and other special cases mentioned in the python docs

--- lib/config_parser.ex
+++ lib/config_parser.ex
@@ -1,18 +1,26 @@
 defmodule ConfigParser do
   defmodule ParseState do
-    defstruct line_number: 1, current_section: :default, result: {:ok, %{}}
+    defstruct line_number: 1,         # What line of the "file" are we parsing
+          current_section: :default,  # Section that definitions go into
+              last_indent: 0,         # The amount of whitespace on the last line
+            continuation?: false,     # Could the line being parsed be a coninuation
+                 last_key: nil,       # If this is a continuation, which key would it continue
+                   result: {:ok, %{}} # The result as it is being built.
 
     def begin_section(parse_state, new_section) do
       # Create a new result, based on the old, with the new section added
       {:ok, section_map} = parse_state.result
 
       # Only add a new section if it's not already there
-      section_atom = String.to_atom(new_section)
-      unless Map.has_key?(section_map, section_atom) do
-        new_result = {:ok, Map.put(section_map, section_atom, %{}) }
+      section_key = String.strip(new_section)
+      unless Map.has_key?(section_map, section_key) do
+        new_result = {:ok, Map.put(section_map, section_key, %{}) }
       end
 
-      %ParseState{current_section: section_atom, result: new_result}
+      %{parse_state | current_section: section_key, 
+                               result: new_result, 
+                        continuation?: false,
+                             last_key: nil}
     end
 
     def define_config(parse_state, key, value) do
@@ -22,19 +30,41 @@ defmodule ConfigParser do
       value_map = section_map[parse_state.current_section]
 
       # create a new set of values by adding the key/value pair passed in
-      new_values = Map.put(value_map, key, value)
+      if value == nil do
+        new_values = Map.put(value_map, String.strip(key), nil)
+      else
+        new_values = Map.put(value_map, String.strip(key), String.strip(value))
+      end
 
       # create a new result replacing the current section with thenew values
       new_result = {:ok, Map.put(section_map, parse_state.current_section, new_values)}
 
-      %{parse_state | result: new_result}
+      %{parse_state | result: new_result, 
+               continuation?: true,
+                    last_key: String.strip(key)}
+    end
+
+    def append_continuation(parse_state, continuation_value) do
+      {:ok, section_map} = parse_state.result
+
+      # pull the values out for the section that's currently being built
+      value_map = section_map[parse_state.current_section]
+
+      # create a new set of values by adding the key/value pair passed in
+      new_value = "#{value_map[parse_state.last_key]} #{continuation_value}"
+
+      define_config(parse_state, parse_state.last_key, new_value)
     end
   end
 
-  @hash_comment_regex ~r/#.*/
-  @semicolon_comment_regex ~r/;.*/
-  @section_regex ~r/\[(\w+)\]/
-  @definition_regex ~r/(\w+)\s*=\s*(\w+)/
+  @hash_comment_regex ~r{#.*}
+  @semicolon_comment_regex ~r{;.*}
+  @section_regex ~r{\[([\w\s]+)\]}
+  
+  @equals_definition_regex ~r{([^=]+)=(.*)}
+  @colon_definition_regex ~r{([^:]+):(.*)}
+
+  @value_like_regex ~r{\s*(\S.*)}
 
   # If the parse state indicates an error we simply skip over lines and propogate
   # the error.
@@ -44,25 +74,49 @@ defmodule ConfigParser do
 
   # Parse a line while the parse state indicates we're in a good state
   def parse_line(line, parse_state = %ParseState{result: {:ok, _}}) do
-    cond do
-      # if we can skip this line (it's empty or a comment) then simply advance the line number
-      can_skip_line(line) ->
-        %{parse_state | line_number: parse_state.line_number + 1}
+    line = strip_inline_comments(line)
 
-      # match a line that begins a new section like "[new_section]"
-      match = Regex.run(@section_regex, line) ->
-        [_, new_section] = match
-        %{ParseState.begin_section(parse_state, new_section) | line_number: parse_state.line_number + 1}
+    # find out how many whitespace characters are on the front of the line
+    indent_level = indent_level(line)
 
-      # match a line that defines a value "key = value"
-      match = Regex.run(@definition_regex, line) ->
-        [_, key, value] = match
-        %{ParseState.define_config(parse_state, key, value) | line_number: parse_state.line_number + 1}
+    if parse_state.continuation? 
+       && indent_level > parse_state.last_indent
+       && (match = Regex.run(@value_like_regex, line)) do
 
-      # Any non-matching lines result in a syntax error
-      true ->
-        %{parse_state | result: {:error, "Syntax Error on line #{parse_state.line_number}"}}
-    end
+      # note that we do not increase the "last indent"
+      %{ParseState.append_continuation(parse_state, String.strip(line)) | line_number: parse_state.line_number + 1, continuation?: true}
+    else
+      cond do
+        # if we can skip this line (it's empty or a comment) then simply advance the line number
+        # and note that the next line can't be a continuation
+        can_skip_line(line) ->
+          %{parse_state | line_number: parse_state.line_number + 1, continuation?: false, last_indent: indent_level}
+
+        # match a line that begins a new section like "[new_section]"
+        match = Regex.run(@section_regex, line) ->
+          [_, new_section] = match
+          %{ParseState.begin_section(parse_state, new_section) | line_number: parse_state.line_number + 1 , last_indent: indent_level}
+
+        # match a line that defines a value "key = value"
+        match = Regex.run(@equals_definition_regex, line) ->
+          [_, key, value] = match
+          %{ParseState.define_config(parse_state, key, value) | line_number: parse_state.line_number + 1, last_indent: indent_level}
+
+        # match a line that defines a value "key : value"
+        match = Regex.run(@colon_definition_regex, line) ->
+          [_, key, value] = match
+          %{ParseState.define_config(parse_state, key, value) | line_number: parse_state.line_number + 1, last_indent: indent_level}
+
+        match = Regex.run(@value_like_regex, line) ->
+          [_, key] = match
+          %{ParseState.define_config(parse_state, key, nil) | continuation?: false, line_number: parse_state.line_number + 1, last_indent: indent_level}
+
+        # match a line that is just "value-ish"
+        # Any non-matching lines result in a syntax error
+        true ->
+          %{parse_state | result: {:error, "Syntax Error on line #{parse_state.line_number}"}}
+      end # cond
+    end # continuation if
   end
 
   def parse(config_file_path) do
@@ -75,6 +129,12 @@ defmodule ConfigParser do
     result
   end
 
+  def indent_level(line) do
+    [_whole, spaces | _rest] = Regex.run(~r{(\s*).*}, line)
+    spaces = String.replace(spaces, "\t", "  ")
+    String.length(spaces)
+  end
+
   def can_skip_line(line) do
     is_comment(line) || is_empty(line)
   end
@@ -85,5 +145,10 @@ defmodule ConfigParser do
 
   def is_empty(line) do
     String.strip(line) == ""
+  end
+
+  def strip_inline_comments(line) do
+    line_list = String.split(line, ";")
+    List.first(line_list)
   end
 end
--- test/config_parser_test.exs
+++ test/config_parser_test.exs
@@ -30,7 +30,14 @@ defmodule ConfigParserTest do
     {:ok, pid} = StringIO.open("[section]\n")
     line_stream = IO.stream(pid, :line)
 
-    assert {:ok, %{section: %{}}} == ConfigParser.parse_stream(line_stream)
+    assert {:ok, %{"section" => %{}}} == ConfigParser.parse_stream(line_stream)
+  end
+
+  test "parses a section name with a space" do
+    {:ok, pid} = StringIO.open("[section with space]\n")
+    line_stream = IO.stream(pid, :line)
+
+    assert {:ok, %{"section with space" => %{}}} == ConfigParser.parse_stream(line_stream)
   end
 
   test "parses a config option into a section" do
@@ -41,19 +48,105 @@ defmodule ConfigParserTest do
       """)
     line_stream = IO.stream(pid, :line)
 
-    assert {:ok, %{section: %{"key" => "value"}}} == ConfigParser.parse_stream(line_stream)
+    assert {:ok, %{"section" => %{"key" => "value"}}} == ConfigParser.parse_stream(line_stream)
   end
 
-  test "recognizes syntax error" do
+  test "allows spaces in the keys" do
     {:ok, pid} = StringIO.open("""
       [section]
       # this is an interesting key value pair
-      this is a bad line
-      key = value
+      spaces in keys=allowed
       """)
     line_stream = IO.stream(pid, :line)
 
-    assert {:error, "Syntax Error on line 3"} == ConfigParser.parse_stream(line_stream)
+    assert {:ok, %{"section" => %{"spaces in keys" => "allowed"}}} == ConfigParser.parse_stream(line_stream)
+  end
+
+  test "allows spaces in the values" do
+    {:ok, pid} = StringIO.open("""
+      [section]
+      # this is an interesting key value pair
+      spaces in values=allowed as well
+      """)
+    line_stream = IO.stream(pid, :line)
+
+    assert {:ok, %{"section" => %{"spaces in values" => "allowed as well"}}} == ConfigParser.parse_stream(line_stream)
+  end
+
+  test "allows spaces around the delimiter" do
+    {:ok, pid} = StringIO.open("""
+      [section]
+      # this is an interesting key value pair
+      spaces around the delimiter = obviously
+      """)
+    line_stream = IO.stream(pid, :line)
+
+    assert {:ok, %{"section" => %{"spaces around the delimiter" => "obviously"}}} == ConfigParser.parse_stream(line_stream)
+  end
+
+  test "allows a colon as the delimiter" do
+    {:ok, pid} = StringIO.open("""
+      [section]
+      # this is an interesting key value pair
+      you can also use : to delimit keys from values
+      """)
+    line_stream = IO.stream(pid, :line)
+
+    assert {:ok, %{"section" => %{"you can also use" => "to delimit keys from values"}}} == ConfigParser.parse_stream(line_stream)
+  end
+
+  test "allows a continuation line" do
+    {:ok, pid} = StringIO.open("""
+      [section]
+      you can also use : to delimit keys from values
+          and add a continuation
+      """)
+    line_stream = IO.stream(pid, :line)
+
+    assert {:ok, %{"section" => %{"you can also use" => "to delimit keys from values and add a continuation"}}} == ConfigParser.parse_stream(line_stream)
+  end
+
+  test "allows a multi-line continuation" do
+    {:ok, pid} = StringIO.open("""
+      [section]
+      you can also use : to delimit keys 
+          from values
+          and add a continuation
+      """)
+    line_stream = IO.stream(pid, :line)
+
+    assert {:ok, %{"section" => %{"you can also use" => "to delimit keys from values and add a continuation"}}} == ConfigParser.parse_stream(line_stream)
+  end
+
+  test "allows empty string thing" do
+    {:ok, pid} = StringIO.open("""
+      [No Values]
+      empty string value here =
+      """)
+    line_stream = IO.stream(pid, :line)
+
+    assert {:ok, %{"No Values" => %{"empty string value here" => ""}}} == ConfigParser.parse_stream(line_stream)
+  end
+
+  test "allows lone keys" do
+    {:ok, pid} = StringIO.open("""
+      [No Values]
+      key_without_value
+      """)
+    line_stream = IO.stream(pid, :line)
+
+    assert {:ok, %{"No Values" => %{"key_without_value" => nil}}} == ConfigParser.parse_stream(line_stream)
+  end
+
+  test "parses a config option with an inline comment" do
+    {:ok, pid} = StringIO.open("""
+      [section]
+      # this is an interesting key value pair
+      key = value ; With a comment
+      """)
+    line_stream = IO.stream(pid, :line)
+
+    assert {:ok, %{"section" => %{"key" => "value"}}} == ConfigParser.parse_stream(line_stream)
   end
 
   test "lines starting with a hash are comments" do
@@ -80,5 +173,70 @@ defmodule ConfigParserTest do
     assert true == ConfigParser.can_skip_line(" \n")
     assert true == ConfigParser.can_skip_line "# this is a comment\n"
     assert true == ConfigParser.can_skip_line "; this is a comment\n"
+  end
+
+  test "parses extended example from python page" do
+    {:ok, pid} = StringIO.open("""
+      [Simple Values]
+      key=value
+      spaces in keys=allowed
+      spaces in values=allowed as well
+      spaces around the delimiter = obviously
+      you can also use : to delimit keys from values
+
+      [All Values Are Strings]
+      values like this: 1000000
+      or this: 3.14159265359
+      are they treated as numbers? : no
+      integers, floats and booleans are held as: strings
+      can use the API to get converted values directly: true
+
+      [Multiline Values]
+      chorus: I'm a lumberjack, and I'm okay,
+          I sleep all night and I work all day
+
+      [No Values]
+      key_without_value
+      empty string value here =
+
+      [You can use comments]
+      # like this
+      ; or this
+
+      # By default only in an empty line.
+      # Inline comments can be harmful because they prevent users
+      # from using the delimiting characters as parts of values.
+      # That being said, this can be customized.
+
+          [Sections Can Be Indented]
+              can_values_be_as_well = True
+              does_that_mean_anything_special = False
+              purpose = formatting for readability
+              multiline_values = are
+                  handled just fine as
+                  long as they are indented
+                  deeper than the first line
+                  of a value
+              # Did I mention we can indent comments, too?
+      """)
+    line_stream = IO.stream(pid, :line)
+
+    assert ConfigParser.parse_stream(line_stream) == {:ok, %{"All Values Are Strings" => %{"are they treated as numbers?" => "no",
+     "can use the API to get converted values directly" => "true",
+     "integers, floats and booleans are held as" => "strings",
+     "or this" => "3.14159265359", "values like this" => "1000000"},
+   "Multiline Values" => %{"chorus" => "I'm a lumberjack, and I'm okay, I sleep all night and I work all day"},
+   "No Values" => %{"empty string value here" => "",
+     "key_without_value" => nil},
+   "Sections Can Be Indented" => %{"can_values_be_as_well" => "True",
+     "does_that_mean_anything_special" => "False",
+     "multiline_values" => "are handled just fine as long as they are indented deeper than the first line of a value",
+     "purpose" => "formatting for readability"},
+   "Simple Values" => %{"key" => "value",
+     "spaces around the delimiter" => "obviously",
+     "spaces in keys" => "allowed", "spaces in values" => "allowed as well",
+     "you can also use" => "to delimit keys from values"},
+   "You can use comments" => %{}}}
+
   end
 end
